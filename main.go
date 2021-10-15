9893a0bb86d39c6e5ff2254f36843a16fd02134b
Markus Wennrich <markus.wennrich@f-i-ts.de>
2021-10-15 15:11:21 +0200

    complete usage stats

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/ceph/go-ceph/rgw/admin"
 
@@ -15,19 +17,29 @@ func main() {
 
 	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
 	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-	//region := os.Getenv("CEPH_REGIOM")
 	endpointURL := os.Getenv("CEPH_ENDPOINT_URL")
+	queryCategories := os.Getenv("QUERY_CATEGORIES")
 
 	if len(accessKey) == 0 {
-		panic("must provide accessKey")
+		panic("must provide AWS_ACCESS_KEY_ID")
+	}
+	if len(secretKey) == 0 {
+		panic("must provide AWS_SECRET_ACCESS_KEY")
+	}
+	if len(endpointURL) == 0 {
+		panic("must provide CEPH_ENDPOINT_URL")
+	}
+	queryCategoriesBool := false
+	if len(queryCategories) != 0 && queryCategories == strings.ToLower("true") {
+		queryCategoriesBool = true
 	}
 
-	co, err := admin.New(endpointURL, accessKey, secretKey, nil)
+	co, err := admin.New(endpointURL, accessKey, secretKey, &http.Client{Timeout: time.Second * 60})
 
 	if err != nil {
 		panic(err)
 	}
-	rgwCollector := newrgwCollector(co)
+	rgwCollector := newrgwCollector(co, queryCategoriesBool)
 	prometheus.MustRegister(rgwCollector)
 
 	http.Handle("/metrics", promhttp.Handler())
--- rgwCollector.go
+++ rgwCollector.go
@@ -9,22 +9,66 @@ import (
 )
 
 type rgwCollector struct {
-	rgwTotalBytes *prometheus.Desc
-	rgw           *admin.API
+	rgwTotalBytesReceived    *prometheus.Desc
+	rgwTotalBytesSent        *prometheus.Desc
+	rgwTotalSuccessfulOps    *prometheus.Desc
+	rgwTotalOps              *prometheus.Desc
+	rgwCategoryBytesReceived *prometheus.Desc
+	rgwCategoryBytesSent     *prometheus.Desc
+	rgwCategorySuccessfulOps *prometheus.Desc
+	rgwCategoryOps           *prometheus.Desc
+	rgw                      *admin.API
+	queryCategories          bool
 }
 
-func newrgwCollector(rgw *admin.API) *rgwCollector {
+func newrgwCollector(rgw *admin.API, queryCategories bool) *rgwCollector {
 	return &rgwCollector{
-		rgwTotalBytes: prometheus.NewDesc("rgw_total_bytes",
-			"Shows rgw total usage in Bytes",
-			[]string{"rgw_total_bytes_received", "user", "endpoint"}, nil,
+		rgwTotalBytesReceived: prometheus.NewDesc("rgw_total_bytes_received",
+			"Shows rgw total received traffic in Bytes",
+			[]string{"user", "endpoint"}, nil,
 		),
-		rgw: rgw,
+		rgwTotalBytesSent: prometheus.NewDesc("rgw_total_bytes_sent",
+			"Shows rgw total sent traffic in Bytes",
+			[]string{"user", "endpoint"}, nil,
+		),
+		rgwTotalOps: prometheus.NewDesc("rgw_total_ops",
+			"Shows rgw total ops",
+			[]string{"user", "endpoint"}, nil,
+		),
+		rgwTotalSuccessfulOps: prometheus.NewDesc("rgw_total_ops_successful",
+			"Shows rgw total sucessfull ops",
+			[]string{"user", "endpoint"}, nil,
+		),
+		rgwCategoryBytesReceived: prometheus.NewDesc("rgw_category_bytes_received",
+			"Shows rgw category received traffic in Bytes",
+			[]string{"user", "endpoint", "category"}, nil,
+		),
+		rgwCategoryBytesSent: prometheus.NewDesc("rgw_category_bytes_sent",
+			"Shows rgw category sent traffic in Bytes",
+			[]string{"user", "endpoint", "category"}, nil,
+		),
+		rgwCategoryOps: prometheus.NewDesc("rgw_category_ops",
+			"Shows rgw category ops",
+			[]string{"user", "endpoint", "category"}, nil,
+		),
+		rgwCategorySuccessfulOps: prometheus.NewDesc("rgw_category_ops_successful",
+			"Shows rgw category sucessfull ops",
+			[]string{"user", "endpoint", "category"}, nil,
+		),
+		rgw:             rgw,
+		queryCategories: queryCategories,
 	}
 }
 
 func (collector *rgwCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- collector.rgwTotalBytes
+	ch <- collector.rgwTotalBytesReceived
+	ch <- collector.rgwTotalBytesSent
+	ch <- collector.rgwTotalOps
+	ch <- collector.rgwTotalSuccessfulOps
+	ch <- collector.rgwCategoryBytesReceived
+	ch <- collector.rgwCategoryBytesSent
+	ch <- collector.rgwCategoryOps
+	ch <- collector.rgwCategorySuccessfulOps
 }
 
 func (collector *rgwCollector) Collect(ch chan<- prometheus.Metric) {
@@ -33,6 +77,18 @@ func (collector *rgwCollector) Collect(ch chan<- prometheus.Metric) {
 		panic(err)
 	}
 	for _, user := range usage.Summary {
-		ch <- prometheus.MustNewConstMetric(collector.rgwTotalBytes, prometheus.GaugeValue, float64(user.Total.BytesReceived), user.User, collector.rgw.Endpoint)
+		ch <- prometheus.MustNewConstMetric(collector.rgwTotalBytesReceived, prometheus.GaugeValue, float64(user.Total.BytesReceived), user.User, collector.rgw.Endpoint)
+		ch <- prometheus.MustNewConstMetric(collector.rgwTotalBytesSent, prometheus.GaugeValue, float64(user.Total.BytesSent), user.User, collector.rgw.Endpoint)
+		ch <- prometheus.MustNewConstMetric(collector.rgwTotalOps, prometheus.GaugeValue, float64(user.Total.Ops), user.User, collector.rgw.Endpoint)
+		ch <- prometheus.MustNewConstMetric(collector.rgwTotalSuccessfulOps, prometheus.GaugeValue, float64(user.Total.SuccessfulOps), user.User, collector.rgw.Endpoint)
+
+		if collector.queryCategories {
+			for _, category := range user.Categories {
+				ch <- prometheus.MustNewConstMetric(collector.rgwCategoryBytesReceived, prometheus.GaugeValue, float64(category.BytesReceived), user.User, collector.rgw.Endpoint, category.Category)
+				ch <- prometheus.MustNewConstMetric(collector.rgwCategoryBytesSent, prometheus.GaugeValue, float64(category.BytesSent), user.User, collector.rgw.Endpoint, category.Category)
+				ch <- prometheus.MustNewConstMetric(collector.rgwCategoryOps, prometheus.GaugeValue, float64(category.Ops), user.User, collector.rgw.Endpoint, category.Category)
+				ch <- prometheus.MustNewConstMetric(collector.rgwCategorySuccessfulOps, prometheus.GaugeValue, float64(category.SuccessfulOps), user.User, collector.rgw.Endpoint, category.Category)
+			}
+		}
 	}
 }
